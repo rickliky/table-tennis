224394b331547b299a7fb79c75a2b1833d484e51
rickliky <likaiyeung@gmail.com>
2026-09-13 13:25:06 +0900

    commit

--- player-profile-supplement.js
+++ player-profile-supplement.js
@@ -1,6 +1,26 @@
 const SURVEY_API_URL = 'https://script.google.com/macros/s/AKfycbx6IaN9YT2a4bv_8W76qtNwkFCjZ_-mODBEMTK9IiJlSi91UCIgJ56MQ4WJqeKK3TiUvA/exec';
 const surveyForm = document.querySelector('#equipment-survey');
 const surveyStatus = document.querySelector('#survey-status');
+const RUBBER_MODELS = {
+  '裏ソフト': [
+    'Butterfly テナジー05 / Tenergy 05', 'Butterfly テナジー05FX / Tenergy 05 FX', 'Butterfly テナジー64 / Tenergy 64', 'Butterfly ディグニクス05 / Dignics 05', 'Butterfly ディグニクス09C / Dignics 09C', 'Butterfly ロゼナ / Rozena',
+    'Nittaku ファスタークG-1 / Fastarc G-1', 'Nittaku ファスタークC-1 / Fastarc C-1', 'Nittaku ハモンドZ2 / Hammond Z2',
+    'Yasaka ラクザ7 / Rakza 7', 'Yasaka ラクザX / Rakza X', 'Yasaka ラクザZ / Rakza Z',
+    'XIOM ヴェガプロ / Vega Pro', 'XIOM ヴェガX / Vega X', 'XIOM ヴェガヨーロッパ / Vega Europe',
+    'Victas V>15 Extra', 'Victas V>15 Limber', 'Tibhar エボリューションMX-P / Evolution MX-P', 'Tibhar エボリューションEL-P / Evolution EL-P',
+    'Donic ブルースターA1 / Bluestar A1', 'andro ラザンターR47 / Rasanter R47', 'Stiga DNA Platinum XH', 'DHS キョウヒョウ3 NEO / Hurricane 3 Neo', '729 バトルII / Battle II'
+  ],
+  '表ソフト': [
+    'Butterfly インパーシャルXS / Impartial XS', 'Butterfly スペクトルS1 / Spectol S1', 'Nittaku モリストSP / Moristo SP', 'Nittaku ハモンドFA / Hammond FA',
+    'Victas VO>102', 'Victas VO>103', 'Victas スピンピップスD3 / Spinpips D3', 'Yasaka ラクザPO / Rakza PO', 'TSP スペクトル / Spectol'
+  ],
+  '粒高': [
+    'Butterfly フェイントロングIII / Feint Long III', 'Nittaku P-1R', 'Victas カールP1V / Curl P1V', 'Victas カールP4V / Curl P4V', 'DHS C8', 'Dawei 388D-1'
+  ],
+  'アンチ': [
+    'Butterfly スーパーアンチ / Super Anti', 'Yasaka アンチパワー / Anti Power', 'Dr. Neubauer ゴリラ / Gorilla', 'Dr. Neubauer バッファロー+ / Buffalo+', 'Der Materialspezialist エレンディル / Ellen DEF'
+  ]
+};
 
 fetch('./public-data.json').then(response => response.ok ? response.json() : Promise.reject()).then(data => {
   const select = document.querySelector('#player-names');
@@ -13,6 +33,17 @@ document.querySelectorAll('[data-other-for]').forEach(field => {
   const input = field.querySelector('input');
   const update = () => { const isOther = select.value === 'Other'; field.hidden = !isOther; input.required = isOther; if (!isOther) input.value = ''; };
   select.addEventListener('change', update); update();
+});
+
+['forehand', 'backhand'].forEach(side => {
+  const type = surveyForm.elements[`${side}Type`];
+  const model = surveyForm.elements[`${side}Model`];
+  type.addEventListener('change', () => {
+    const models = RUBBER_MODELS[type.value] || [];
+    model.innerHTML = `<option value="">${type.value ? 'モデルを選択 / Select model' : '先に種類を選択 / Select type first'}</option>${models.map(name => `<option>${name}</option>`).join('')}<option value="Other">その他 / Other</option>`;
+    model.dispatchEvent(new Event('change'));
+  });
+  type.dispatchEvent(new Event('change'));
 });
 
 surveyForm.addEventListener('submit', async event => {
