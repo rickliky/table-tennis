91394f00f11113af2bc52a0beed35286f1b6d8a5
rickliky <likaiyeung@gmail.com>
2026-09-17 10:03:05 +0900

    fix: stop forcing gradeHistory into every diff - only show actual changes

--- worker/src/diff.js
+++ worker/src/diff.js
@@ -1,4 +1,4 @@
 export function createDiff(before, after) {
   const keys = new Set([...Object.keys(before || {}), ...Object.keys(after || {})]);
-  return [...keys].filter(key => key === 'gradeHistory' || JSON.stringify(before?.[key]) !== JSON.stringify(after?.[key])).map(field => ({ field, before: before?.[field] ?? '', after: after?.[field] ?? '' }));
+  return [...keys].filter(key => JSON.stringify(before?.[key]) !== JSON.stringify(after?.[key])).map(field => ({ field, before: before?.[field] ?? '', after: after?.[field] ?? '' }));
 }
