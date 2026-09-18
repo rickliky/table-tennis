93282f1db79f25e736d420a4c04c8d8db897a1bc
rickliky <rickliky@users.noreply.github.com>
2026-09-18 03:16:49 +0000

    deploy: e1845ada20b55a78ae23971c85bad946c0da9f45

---
+++ uat/static-data.js
@@ -0,0 +1,290 @@
+window.LK_STATIC = {
+  "playingStyles": [
+    {
+      "id": "PS-001",
+      "name": "ドライブ攻撃型 / Topspin attacker"
+    },
+    {
+      "id": "PS-002",
+      "name": "カット主戦型 / Defensive chopper"
+    },
+    {
+      "id": "PS-003",
+      "name": "表ソフト攻撃型 / Short-pips attacker"
+    },
+    {
+      "id": "PS-004",
+      "name": "前陣速攻型 / Close-to-table attacker"
+    },
+    {
+      "id": "PS-005",
+      "name": "異質攻撃型 / Combination attacker"
+    },
+    {
+      "id": "PS-006",
+      "name": "オールラウンド型 / All-round"
+    }
+  ],
+  "grips": [
+    {
+      "id": "GH-001",
+      "name": "シェークハンド / Shakehand"
+    },
+    {
+      "id": "GH-002",
+      "name": "ペンホルダー / Penhold"
+    }
+  ],
+  "playingHands": [
+    {
+      "id": "PH-001",
+      "name": "右",
+      "nameEn": "Right"
+    },
+    {
+      "id": "PH-002",
+      "name": "左",
+      "nameEn": "Left"
+    }
+  ],
+  "rubberTypes": [
+    {
+      "id": "RT-001",
+      "name": "裏ソフト",
+      "nameEn": "Inverted"
+    },
+    {
+      "id": "RT-002",
+      "name": "表ソフト",
+      "nameEn": "Short pips"
+    },
+    {
+      "id": "RT-003",
+      "name": "粒高",
+      "nameEn": "Long pips"
+    },
+    {
+      "id": "RT-004",
+      "name": "アンチ",
+      "nameEn": "Anti-spin"
+    },
+    {
+      "id": "RT-005",
+      "name": "一枚",
+      "nameEn": "OX pips"
+    }
+  ],
+  "schoolLevels": [
+    {
+      "id": "SL-001",
+      "name": "小学生",
+      "nameEn": "Elementary school"
+    },
+    {
+      "id": "SL-002",
+      "name": "中学生",
+      "nameEn": "Middle school"
+    },
+    {
+      "id": "SL-003",
+      "name": "高校生",
+      "nameEn": "High school"
+    },
+    {
+      "id": "SL-004",
+      "name": "一般",
+      "nameEn": "Adult"
+    }
+  ],
+  "genders": [
+    {
+      "id": "GD-001",
+      "name": "Male",
+      "nameJa": "男性"
+    },
+    {
+      "id": "GD-002",
+      "name": "Female",
+      "nameJa": "女性"
+    },
+    {
+      "id": "GD-003",
+      "name": "Other",
+      "nameJa": "その他"
+    }
+  ],
+  "statuses": [
+    {
+      "id": "ST-001",
+      "name": "Active",
+      "nameJa": "活動中"
+    },
+    {
+      "id": "ST-002",
+      "name": "Inactive",
+      "nameJa": "非活動"
+    }
+  ],
+  "tournamentRounds": [
+    {
+      "id": "TR-001",
+      "name": "1回戦",
+      "nameEn": "Round 1"
+    },
+    {
+      "id": "TR-002",
+      "name": "2回戦",
+      "nameEn": "Round 2"
+    },
+    {
+      "id": "TR-003",
+      "name": "3回戦",
+      "nameEn": "Round 3"
+    },
+    {
+      "id": "TR-004",
+      "name": "4回戦",
+      "nameEn": "Round 4"
+    },
+    {
+      "id": "TR-005",
+      "name": "5回戦",
+      "nameEn": "Round 5"
+    },
+    {
+      "id": "TR-006",
+      "name": "6回戦",
+      "nameEn": "Round 6"
+    },
+    {
+      "id": "TR-007",
+      "name": "最終ラウンド",
+      "nameEn": "Finals"
+    },
+    {
+      "id": "TR-008",
+      "name": "グループステージ",
+      "nameEn": "Group stage"
+    }
+  ],
+  "tournamentResults": [
+    {
+      "id": "TP-001",
+      "name": "未設定 / Not specified"
+    },
+    {
+      "id": "TP-002",
+      "name": "優勝",
+      "nameEn": "Champion"
+    },
+    {
+      "id": "TP-003",
+      "name": "準優勝",
+      "nameEn": "Runner-up"
+    },
+    {
+      "id": "TP-004",
+      "name": "3位",
+      "nameEn": "3rd place"
+    },
+    {
+      "id": "TP-005",
+      "name": "4位",
+      "nameEn": "4th place"
+    },
+    {
+      "id": "TP-006",
+      "name": "5位",
+      "nameEn": "5th place"
+    },
+    {
+      "id": "TP-007",
+      "name": "6位",
+      "nameEn": "6th place"
+    },
+    {
+      "id": "TP-008",
+      "name": "7位",
+      "nameEn": "7th place"
+    },
+    {
+      "id": "TP-009",
+      "name": "8位",
+      "nameEn": "8th place"
+    },
+    {
+      "id": "TP-010",
+      "name": "デフォルト",
+      "nameEn": "Default"
+    }
+  ],
+  "resultStatuses": [
+    {
+      "id": "RS-001",
+      "name": "Completed",
+      "nameJa": "完了"
+    },
+    {
+      "id": "RS-002",
+      "name": "Incomplete",
+      "nameJa": "未完了"
+    },
+    {
+      "id": "RS-003",
+      "name": "Void",
+      "nameJa": "無効"
+    },
+    {
+      "id": "RS-004",
+      "name": "Verified",
+      "nameJa": "確認済み"
+    },
+    {
+      "id": "RS-005",
+      "name": "Transcribed - review",
+      "nameJa": "転記済み - 要確認"
+    },
+    {
+      "id": "RS-006",
+      "name": "Complete",
+      "nameJa": "完了"
+    },
+    {
+      "id": "RS-007",
+      "name": "Draw",
+      "nameJa": "引き分け"
+    }
+  ],
+  "grades": [
+    {
+      "id": "GR-001",
+      "name": "1年生",
+      "nameEn": "1st year"
+    },
+    {
+      "id": "GR-002",
+      "name": "2年生",
+      "nameEn": "2nd year"
+    },
+    {
+      "id": "GR-003",
+      "name": "3年生",
+      "nameEn": "3rd year"
+    },
+    {
+      "id": "GR-004",
+      "name": "4年生",
+      "nameEn": "4th year"
+    },
+    {
+      "id": "GR-005",
+      "name": "5年生",
+      "nameEn": "5th year"
+    },
+    {
+      "id": "GR-006",
+      "name": "6年生",
+      "nameEn": "6th year"
+    }
+  ]
+};
