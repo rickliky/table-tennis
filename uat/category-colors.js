2315a6a684aede86a95705b11167c80ff935330a
rickliky <rickliky@users.noreply.github.com>
2026-09-21 03:22:44 +0000

    deploy: ba9fe4a73bf5c251f363765f595aaceee2c9764f

--- uat/category-colors.js
+++ uat/category-colors.js
@@ -1,8 +1,8 @@
 window.categoryDistribution = (() => {
   const colors = {
-    '小学生':'#d6a516', 'Elementary School':'#d6a516',
-    '中学生':'#54b8d1', 'Junior High School':'#54b8d1',
-    '高校生':'#f6f0e2', 'High School':'#f6f0e2',
+    '小学生':'#d6a516', 'Elementary School':'#d6a516', 'Elementary school':'#d6a516',
+    '中学生':'#54b8d1', 'Junior High School':'#54b8d1', 'Middle school':'#54b8d1',
+    '高校生':'#f6f0e2', 'High School':'#f6f0e2', 'High school':'#f6f0e2',
     '一般':'#8c423a', 'Adult':'#8c423a',
     '未設定':'#6e6a62', 'Unassigned':'#6e6a62'
   };
